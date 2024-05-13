3bc2b814683cc4cac0dba79986c219d012d88fbd
Nil SJ <niladri123@gmail.com>
2024-05-13 13:48:45 -0400

    Checking it again

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -101,7 +101,7 @@ public class JobData {
                 if (cellContent.toLowerCase().contains(value.toLowerCase())) {
                     if (!foundJobs.contains(row)) { // to avoid duplicates
                         foundJobs.add(row);
-                        //break;
+                        break;
                     }
                 }
             }
